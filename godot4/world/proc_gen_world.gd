b74ee7fc78a4d733847f8e1e2dab83d925124be0
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2026-05-14 07:41:36 +0200

    Version 0.2.0
     Use different seed for every world generation
     Support arrow key for movement
     Add quit game feature

--- godot4/world/proc_gen_world.gd
+++ godot4/world/proc_gen_world.gd
@@ -1,28 +1,35 @@
 class_name ProcGenWorld
 extends Node2D
 
-@onready var tile_map = $TileMap
 
 @export var noise_texture : NoiseTexture2D
 @export var tree_noise_texture : NoiseTexture2D
+@export var seed_value: int = 0
+
 
 var width : int = 300
 var height : int =  300
 
+
 var noise : Noise
 var tree_noise : Noise
+
 
 var water_tile_atlas = Vector2i(0,1)
 var tree_atlas = Vector2i(12,2)
 var tree_atlas2 = Vector2i(15,6)
 
-#TERRAIN ARR
+
 var sand_arr = []
 var grass_arr = []
 var dirt_arr = []
 var cliff_arr = []
 
-#LAYERS
+var random_grass_atlas_arr = [Vector2i(1, 0), Vector2i(2, 0), Vector2i(3, 0), Vector2i(4, 0), Vector2i(5, 0)]
+
+
+@onready var tile_map = $TileMap
+@onready var camera_2d = $Player/Camera2D
 @onready var water_layer: TileMapLayer = $TileMap/water_layer
 @onready var ground_layer: TileMapLayer = $TileMap/ground_layer
 @onready var ground_2_layer: TileMapLayer = $TileMap/ground2_layer
@@ -30,21 +37,20 @@ var cliff_arr = []
 @onready var environment_layer: TileMapLayer = $TileMap/environment_layer
 
 
-var random_grass_atlas_arr = [Vector2i(1,0),Vector2i(2,0),Vector2i(3,0),Vector2i(4,0),Vector2i(5,0)]
-@onready var camera_2d = $Player/Camera2D
-
-
 func _ready() -> void:
 	if not OS.has_feature("editor"):
 		get_window().mode = Window.MODE_EXCLUSIVE_FULLSCREEN
+
 	noise = noise_texture.noise
 	tree_noise = tree_noise_texture.noise
-	generate_world()
+
+	_generate_world()
 
 
-func generate_world() -> void:
+func _generate_world() -> void:
 	var noise_val: float
 	var tree_noise_val: float
+	_generate_seed()
 	
 	@warning_ignore("integer_division")
 	for x in range(-width/2, width/2):
@@ -83,6 +89,14 @@ func generate_world() -> void:
 	ground_layer.set_cells_terrain_connect(grass_arr, 1,0)
 	cliff_layer.set_cells_terrain_connect(cliff_arr, 4,0)
 
+
+func _generate_seed() -> void:
+	if seed_value == 0:
+		seed_value = randi()
+	noise.seed = seed_value
+	tree_noise.seed = seed_value
+
+
 func _input(_event):
 	if Input.is_action_just_pressed("zoom_in"):
 		var zoom_val =camera_2d.zoom.x + 0.1
@@ -92,5 +106,6 @@ func _input(_event):
 		var zoom_val =camera_2d.zoom.x - 0.1
 		if zoom_val == 0:
 			zoom_val =camera_2d.zoom.x - 0.2
-			
 		camera_2d.zoom = Vector2(zoom_val, zoom_val)
+	elif Input.is_action_just_pressed("quit"):
+		get_tree().quit(0)
