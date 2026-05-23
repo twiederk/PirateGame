cf16b35bf366e51be3e6ba5968209fba832572aa
IN57KC <torsten.wiederkehr@ing.com>
2026-05-23 18:26:34 +0200

    Improved layout

--- godot4/city/city.gd
+++ godot4/city/city.gd
@@ -9,11 +9,14 @@ var city: Dictionary = trading_system.cities[player.current_city]
 @onready var city_name = $CenterContainer/VBoxContainer/CityName
 @onready var player_gold = $CenterContainer/VBoxContainer/PlayerGold
 @onready var player_weight = $CenterContainer/VBoxContainer/PlayerWeight
-@onready var city_fish_amount = $CenterContainer/VBoxContainer/CityGoodFish/CityFishAmount
-@onready var city_fish_price = $CenterContainer/VBoxContainer/CityGoodFish/CityFishPrice
-@onready var city_grain_amount = $CenterContainer/VBoxContainer/CityGoodGrain/CityGrainAmount
-@onready var city_grain_price = $CenterContainer/VBoxContainer/CityGoodGrain/CityGrainPrice
-@onready var buy_fish_button = $CenterContainer/VBoxContainer/CityGoodFish/BuyFishButton
+
+@onready var good_name = $CenterContainer/VBoxContainer/GoodRow/GoodName
+@onready var good_price = $CenterContainer/VBoxContainer/GoodRow/GoodPrice
+@onready var player_amount = $CenterContainer/VBoxContainer/GoodRow/PlayerAmount
+@onready var city_amount = $CenterContainer/VBoxContainer/GoodRow/CityAmount
+@onready var buy_button = $CenterContainer/VBoxContainer/GoodRow/BuyButton
+@onready var sell_button = $CenterContainer/VBoxContainer/GoodRow/SellButton
+
 
 
 func _ready():
@@ -28,26 +31,20 @@ func _update_gui() -> void:
 	city_name.text = city.name
 	player_gold.text = "Gold: " + str(player.gold)
 	player_weight.text = "Laderaum: " + str(trading_system.get_used_capacity()) + " / " + str(player.cargo_capacity)
-	city_fish_amount.text = str(city.market.fish.stock)
-	city_fish_price.text = str(trading_system.get_price(city, "fish")) + "$"
-	city_grain_amount.text = str(city.market.grain.stock)
-	city_grain_price.text = str(trading_system.get_price(city, "grain")) + "$"
-
+	
+	good_name.text = "Fisch"
+	good_price.text = str(trading_system.get_price(city, "fish")) + "$"
+	player_amount.text = str(player.inventory.fish)
+	city_amount.text = str(city.market.fish.stock)
 
 func _on_buy_fish_button_pressed():
 	trading_system.buy("fish", 1)
-	player_gold.text = "Gold: " + str(player.gold)
-	player_weight.text = "Laderaum: " + str(trading_system.get_used_capacity()) + " / " + str(player.cargo_capacity)
-	city_fish_amount.text = str(city.market.fish.stock)
-	city_fish_price.text = str(trading_system.get_price(city, "fish")) + "$"
+	_update_gui()
 
 
 func _on_sell_fish_button_pressed():
 	trading_system.sell("fish", 1)
-	player_gold.text = "Gold: " + str(player.gold)
-	player_weight.text = "Laderaum: " + str(trading_system.get_used_capacity()) + " / " + str(player.cargo_capacity)
-	city_fish_amount.text = str(city.market.fish.stock)
-	city_fish_price.text = str(trading_system.get_price(city, "fish")) + "$"
+	_update_gui()
 
 
 func _on_travel_button_pressed():
