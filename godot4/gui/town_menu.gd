1457310581c66a0f031b3e0c17320f7143f8ef43
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2026-05-25 14:29:45 +0200

    Renamed city to town

--- godot4/gui/town_menu.gd
+++ godot4/gui/town_menu.gd
@@ -8,7 +8,7 @@ signal town_left
 var trading_system: TradingSystem = TradingSystem.new()
 
 var player: Dictionary = trading_system.player
-var city: Dictionary = trading_system.cities[player.current_city]
+var town: Dictionary = trading_system.cities[player.current_city]
 
 @onready var city_name = $CenterContainer/VBoxContainer/CityName
 @onready var player_gold = $CenterContainer/VBoxContainer/PlayerGold
@@ -33,15 +33,15 @@ func _process(delta: float):
 
 
 func _update_gui() -> void:
-	city_name.text = city.name
-	background.self_modulate = city.background_color
+	city_name.text = town.name
+	background.self_modulate = town.background_color
 	player_gold.text = "Gold: " + str(player.gold)
 	player_weight.text = "Laderaum: " + str(trading_system.get_used_capacity()) + " / " + str(player.cargo_capacity)
 	
 	good_name.text = "Fisch"
-	good_price.text = str(trading_system.get_price(city, "fish")) + "$"
+	good_price.text = str(trading_system.get_price(town, "fish")) + "$"
 	player_amount.text = str(player.inventory.fish)
-	city_amount.text = str(city.market.fish.stock)
+	city_amount.text = str(town.market.fish.stock)
 
 func _on_buy_fish_button_pressed():
 	trading_system.buy("fish", 1)
