d0089b4a7ecf978a1be20fc5b5731b101af0e3d2
IN57KC <torsten.wiederkehr@ing.com>
2026-06-12 12:41:02 +0200

    Added load game to start menu

--- godot4/gui/start_menu.gd
+++ godot4/gui/start_menu.gd
@@ -5,6 +5,15 @@ extends Control
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://world/main.tscn")
 
+func _on_load_button_pressed():
+	SaveManager.load_game_state = {
+		"world_seed": 67890,
+		"player": {
+			"gold": 99,
+		}
+	}
+	get_tree().change_scene_to_file("res://world/main.tscn")
+
 
 func _on_quit_button_pressed():
 	get_tree().quit()
