3fd26ad4b3d17944167c7db5e74c872f75350597
IN57KC <torsten.wiederkehr@ing.com>
2026-06-19 17:03:31 +0200

    Added TownResource.type

--- godot4/world/town_resource.gd
+++ godot4/world/town_resource.gd
@@ -1,6 +1,11 @@
 class_name TownResource
 extends Resource
 
+
+enum Type { Habor, Farm, Woodcamp }
+
+
+@export var type: Type
 @export var name: String
 @export var background_color: Color
 @export var produces: Array[GoodResource]
