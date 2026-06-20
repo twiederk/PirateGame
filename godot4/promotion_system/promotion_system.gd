16278fb8bba0688f9421839e6fd72add5927a294
IN57KC <torsten.wiederkehr@ing.com>
2026-06-20 06:43:51 +0200

    Started implementation of promotion system

---
+++ godot4/promotion_system/promotion_system.gd
@@ -0,0 +1,2 @@
+class_name PromotionSystem
+extends Node
---
+++ godot4/test/test_promotion_system.gd
@@ -0,0 +1,26 @@
+extends GutTest
+
+var promotion_system = null
+var player: Player = null
+
+
+func before_each():
+	player = Player.new()
+	promotion_system = PromotionSystem.new()
+
+
+func after_each():
+	player.free()
+	promotion_system.free()
+
+
+func test_promote_to_haendler_at_500_gold():
+	# arrange
+	player.current_title = "Krämer"
+	player.gold = 500
+
+	# act
+	promotion_system.evaluate(player)
+
+	# assert
+	assert_eq(player.current_title, "Händler", "Player title should be promoted to Händler at 500 gold")
