b2fab0d7d478827dedcd2a72840d826052618b17
IN57KC <torsten.wiederkehr@ing.com>
2026-06-20 07:09:49 +0200

    Implemented PromotionSystem.get_gold_title

--- godot4/promotion_system/promotion_system.gd
+++ godot4/promotion_system/promotion_system.gd
@@ -1,2 +1,21 @@
 class_name PromotionSystem
 extends Node
+
+
+const GOLD_TITLES: Dictionary = {
+	0: "Krämer",
+	500: "Händler",
+	1_000: "Großhändler",
+	2_500: "Kaufmann",
+	5_000: "Großkaufmann",
+	7_500: "Zunftmeister",
+	10_000: "Handelsfürst",
+}
+
+
+func get_gold_title(gold: int) -> String:
+	var gold_title = GOLD_TITLES[0]
+	for gold_threadhold in GOLD_TITLES:
+		if gold >= gold_threadhold:
+			gold_title = GOLD_TITLES[gold_threadhold]
+	return gold_title
--- godot4/test/test_promotion_system.gd
+++ godot4/test/test_promotion_system.gd
@@ -5,22 +5,30 @@ var player: Player = null
 
 
 func before_each():
-	player = Player.new()
 	promotion_system = PromotionSystem.new()
+	player = Player.new()
 
 
 func after_each():
-	player.free()
 	promotion_system.free()
+	player.free()
 
 
-func test_promote_to_haendler_at_500_gold():
-	# arrange
-	player.current_title = "Krämer"
-	player.gold = 500
-
+func test_get_gold_title():
 	# act
-	promotion_system.evaluate(player)
-
+	var gold_title = promotion_system.get_gold_title(100)
+	
 	# assert
-	assert_eq(player.current_title, "Händler", "Player title should be promoted to Händler at 500 gold")
+	assert_eq(gold_title, "Krämer")
+
+
+#func test_promote_to_haendler_at_500_gold():
+	## arrange
+	#player.current_title = "Krämer"
+	#player.gold = 500
+#
+	## act
+	#promotion_system.evaluate(player)
+#
+	## assert
+	#assert_eq(player.current_title, "Händler", "Player title should be promoted to Händler at 500 gold")
