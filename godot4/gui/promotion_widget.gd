7f2b4499a613092d802ec7ad539b97a9c4d61cf6
IN57KC <torsten.wiederkehr@ing.com>
2026-06-20 15:50:27 +0200

    Hide promotion message after 5 secs

--- godot4/gui/promotion_widget.gd
+++ godot4/gui/promotion_widget.gd
@@ -2,9 +2,14 @@ class_name PromotionWidget
 extends Control
 
 @onready var promotion_label = $PromotionLabel
+@onready var timer = $Timer
 
 
 func show_promotion(new_rank: PrestigeRank) -> void:
+	promotion_label.text = "Du hat den neuen Titel: " + new_rank.title + " erhalten!" 
+	timer.start()
 	show()
-	var message = "Du hat den neuen Titel: " + new_rank.title + " erhalten!" 
-	promotion_label.text = message
+
+
+func _on_timer_timeout():
+	hide()
