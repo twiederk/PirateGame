61e2c3a81ec0e9be32c496f6cf1c22f9bf5ae0aa
IN57KC <torsten.wiederkehr@ing.com>
2026-06-25 16:37:26 +0200

    Implemented ProcGenWorldSerializer

--- godot4/test/test_proc_gen_world_serializer.gd
+++ godot4/test/test_proc_gen_world_serializer.gd
@@ -58,3 +58,61 @@ func test_get_save_data():
 	# tear down
 	towns_root.free()
 	goods_root.free()
+
+
+func test_set_save_data():
+	# arrange
+	var towns_root = Node2D.new()
+	var town = Town.new()
+	var town_item = TradingItem.new(GOOD_FISH, 1)
+	town_item.cached_stock = 2
+	town_item.last_updated = 3.0
+	town.add_trading_item(town_item)
+	towns_root.add_child(town)
+	proc_gen_world.towns = towns_root
+	
+	var goods_root = Node2D.new()
+	proc_gen_world.goods = goods_root
+
+	var save_data = {
+		"world": {
+			"spawn_accumulator": 0.5,
+			"towns": [
+				{
+					"visited": true,
+					"inventory": {
+						"1": {
+							"stock": 50,
+							"cached_stock": 45,
+							"last_updated": 1234.5,
+						}
+					}
+				}
+			],
+			"goods": [
+				{
+					"resource_path": GOOD_FISH.resource_path,
+					"position": {"x": 10, "y": 20},
+				}
+			]
+		}
+	}
+
+	# act
+	proc_gen_world_serializer.set_save_data(proc_gen_world, save_data)
+
+	# assert
+	assert_eq(proc_gen_world.spawn_accumulator, 0.5, "should restore spawn accumulator")
+	assert_true(town.get_visited(), "should restore visited")
+	assert_eq(town_item.stock, 50, "should restore town item stock")
+	assert_eq(town_item.cached_stock, 45, "should restore town item cached_stock")
+	assert_eq(town_item.last_updated, 1234.5, "should restore town item last_updated")
+	var goods = proc_gen_world.get_goods()
+	assert_eq(goods.size(), 1, "Should restore goods")
+	var good = goods[0]
+	assert_eq(good.good_resource.resource_path, GOOD_FISH.resource_path, "Should restore good resource of good")
+	assert_eq(good.global_position, Vector2(10, 20), "Should restore position of good")
+	
+	# tear down
+	towns_root.free()
+	goods_root.free()
--- godot4/world/proc_gen_world_serializer.gd
+++ godot4/world/proc_gen_world_serializer.gd
@@ -1,5 +1,8 @@
 class_name ProcGenWorldSerializer
 
+const GoodScene = preload("res://world/good.tscn")
+
+
 func get_save_data(proc_gen_world: ProcGenWorld) -> Dictionary:
 	var world_data = {
 		"seed_value": proc_gen_world.seed_value,
@@ -40,3 +43,37 @@ func _serialize_good_save_data(good: Good) -> Dictionary:
 			"y": good.position.y,
 		}
 	}
+
+
+func set_save_data(proc_gen_world: ProcGenWorld, save_data: Dictionary) -> void:
+	var world_data: Dictionary = save_data.world
+	if world_data.has("spawn_accumulator"):
+		proc_gen_world.spawn_accumulator = world_data.spawn_accumulator
+
+	var towns_data: Array = world_data.towns
+	var generated_towns : Array[Town] = proc_gen_world.get_towns()
+	for i in range(towns_data.size()):
+		var town_data: Dictionary = towns_data[i]
+		var current_town = generated_towns[i]
+		if town_data.has("visited"):
+			current_town.set_visited(town_data.visited)
+		_restore_town_inventory_from_save(current_town, town_data.inventory)
+
+	if world_data.has("goods"):
+		var goods_data: Array = world_data.goods
+		for i in range(goods_data.size()):
+			var good_data: Dictionary = goods_data[i]
+			var good: Good = GoodScene.instantiate()
+			var good_resource = load(good_data.resource_path)
+			good.good_resource = good_resource
+			var pos = Vector2i(int(good_data.position.x), int(good_data.position.y))
+			good.global_position = pos
+			proc_gen_world.goods.add_child(good)
+
+
+func _restore_town_inventory_from_save(town: Town, inventory_data: Dictionary) -> void:
+	for item in town.get_trading_items():
+		var item_save_data = inventory_data[str(item.good_id)]
+		item.stock = item_save_data.stock
+		item.cached_stock = item_save_data.cached_stock
+		item.last_updated = item_save_data.last_updated
