dd6eba3d9903a8759ad2994f5fa4edfd05ff1c68
IN57KC <torsten.wiederkehr@ing.com>
2026-06-26 08:34:54 +0200

    Added test for TradingSystemSerializer.set_save_data

--- godot4/test/test_trading_system_serializer.gd
+++ godot4/test/test_trading_system_serializer.gd
@@ -43,3 +43,17 @@ func test_set_save_data():
 	# assert
 	assert_eq(trading_system.current_game_time, 456.75, "Should restore current_game_time from save data")
 	assert_eq(trading_system.accumulator, 9.5, "Should restore accumulator from save data")
+	
+	
+func test_set_save_data_missing_data_use_defaults():
+	# arrange
+	var save_data = {
+		"trading_system": { }
+	}
+	
+	# act
+	trading_system_serializer.set_save_data(trading_system, save_data)
+
+	# assert
+	assert_eq(trading_system.current_game_time, 0.0, "Should use default data for current_game_time")
+	assert_eq(trading_system.accumulator, 0.0, "Should use default data for accumulator")
--- godot4/trading_system/trading_system_serializer.gd
+++ godot4/trading_system/trading_system_serializer.gd
@@ -10,6 +10,10 @@ func get_save_data(trading_system: TradingSystem) -> Dictionary:
 
 
 func set_save_data(trading_system: TradingSystem, save_data: Dictionary) -> void:
+	if not save_data.has("trading_system"):
+		return
 	var trading_system_data = save_data.trading_system
-	trading_system.current_game_time = trading_system_data.current_game_time
-	trading_system.accumulator = trading_system_data.accumulator
+	if trading_system_data.has("current_game_time"):
+		trading_system.current_game_time = trading_system_data.current_game_time
+	if trading_system_data.has("accumulator"):
+		trading_system.accumulator = trading_system_data.accumulator
