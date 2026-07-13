68e3d7785557060cf68250cd80432292b1a0d119
IN57KC <torsten.wiederkehr@ing.com>
2026-07-13 13:17:52 +0200

    Created RaidersGenerator

--- godot4/world/goods_generator.gd
+++ godot4/world/goods_generator.gd
@@ -1,11 +1,10 @@
 class_name GoodsGenerator
+extends Generator
 
 
 @export var fish_percentage: float = 0.125
 @export var grain_percentage: float = 0.05
 @export var wood_percentage: float = 0.05
-
-const INVALID_TILE_POSITION: Vector2i = Vector2i(-1, -1)
 
 const GOOD_FISH = preload("res://trading_system/good_fish.tres")
 const GOOD_GRAIN = preload("res://trading_system/good_grain.tres")
@@ -51,32 +50,3 @@ func _generate_goods_of_type(good_resource: GoodResource, goods_to_generate: int
 
 func _get_goods_by_type(all_goods: Array[Good], good_resource: GoodResource) -> Array[Good]:
 	return all_goods.filter(func(good): return good.good_resource == good_resource)
-
-
-func _pick_hidden_tile_position(viewport: Viewport, positions: Array[Vector2i]) -> Vector2i:
-	if positions.is_empty():
-		return INVALID_TILE_POSITION
-
-	var hidden_positions: Array[Vector2i] = positions.filter(func(pos): return not _is_tile_position_visible(viewport, pos))
-	if hidden_positions.is_empty():
-		return INVALID_TILE_POSITION
-
-	return hidden_positions.pick_random()
-
-
-func _is_tile_position_visible(viewport: Viewport, tile_position: Vector2i) -> bool:
-	if viewport == null:
-		return false
-
-	var camera: Camera2D = viewport.get_camera_2d()
-	if camera == null:
-		return false
-
-	var viewport_size: Vector2 = viewport.get_visible_rect().size
-	var visible_size: Vector2 = viewport_size * camera.zoom
-	var visible_origin: Vector2 = camera.get_screen_center_position() - (visible_size * 0.5)
-	var visible_rect := Rect2(visible_origin, visible_size)
-
-	var tile_size: Vector2i = ProcGenWorld.TILE_SIZE
-	var tile_world_rect := Rect2(Vector2(tile_position * tile_size), Vector2(tile_size))
-	return visible_rect.intersects(tile_world_rect)
--- godot4/world/raiders_generator.gd
+++ godot4/world/raiders_generator.gd
@@ -1,1 +1,32 @@
 class_name RaidersGenerator
+extends Generator
+
+
+@export var raider_percentage: float = 0.02
+
+const RAIDER_DISTANCE: float = 350.0
+
+const RaiderScene = preload("res://world/raider.tscn")
+
+
+func generate_raiders(proc_gen_world: ProcGenWorld, player_position: Vector2) -> void:
+	var width = proc_gen_world.width
+	var viewport = proc_gen_world.get_viewport()
+	var grass_arr = proc_gen_world.grass_arr
+	var raiders_root = proc_gen_world.raiders
+	
+	var max_raiders = int(width * raider_percentage)
+	var raiders_to_generate = max_raiders - proc_gen_world.get_raiders().size()
+	for i in range(raiders_to_generate):
+		var spawn_position = _pick_hidden_tile_position(viewport, grass_arr)
+		if spawn_position == INVALID_TILE_POSITION:
+			continue
+		if _in_raider_distance(player_position, spawn_position):
+			continue
+		var raider: Raider = RaiderScene.instantiate() 
+		raider.global_position = spawn_position * ProcGenWorld.TILE_SIZE
+		raiders_root.add_child(raider)
+
+
+func _in_raider_distance(player_position, spawn_position) -> bool:
+	return player_position.distance_to(Vector2(spawn_position * ProcGenWorld.TILE_SIZE)) <= RAIDER_DISTANCE
