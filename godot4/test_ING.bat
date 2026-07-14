f2061773f7473d5f0c30bb1dd02320dcb0c70e9a
IN57KC <torsten.wiederkehr@ing.com>
2026-07-14 05:46:40 +0200

    Updated batch commands to use Godot 4.7

--- godot4/test_ING.bat
+++ godot4/test_ING.bat
@@ -1,2 +1,2 @@
-"C:\Users\IN57KC\data\Programme\Godot\Godot_v4.6-stable_win64.exe" --path . --headless --script addons/gut/gut_cmdln.gd -gdir=test/ -gexit -glog=1
+"C:\Users\IN57KC\data\Programme\Godot\Godot_v4.7-stable_win64.exe" --path . --headless --script addons/gut/gut_cmdln.gd -gdir=test/ -gexit -glog=1
 pause
