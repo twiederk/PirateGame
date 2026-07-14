d16fced4ad37c06799c1732e80ff613e46400765
IN57KC <torsten.wiederkehr@ing.com>
2026-07-14 08:55:37 +0200

    Raides does not spawn on cliffs

--- godot4/world/raiders_generator.gd
+++ godot4/world/raiders_generator.gd
@@ -12,13 +12,13 @@ const RaiderScene = preload("res://world/raider.tscn")
 func generate_raiders(proc_gen_world: ProcGenWorld, player_position: Vector2) -> void:
 	var width = proc_gen_world.width
 	var viewport = proc_gen_world.get_viewport()
-	var grass_arr = proc_gen_world.grass_arr
 	var raiders_root = proc_gen_world.raiders
 	
 	var max_raiders = int(width * raider_percentage)
 	var raiders_to_generate = max_raiders - proc_gen_world.get_raiders().size()
+	var raiders_positions = _create_raiders_positions(proc_gen_world)
 	for i in range(raiders_to_generate):
-		var spawn_position = _pick_hidden_tile_position(viewport, grass_arr)
+		var spawn_position = _pick_hidden_tile_position(viewport, raiders_positions)
 		if spawn_position == INVALID_TILE_POSITION:
 			continue
 		if _in_raider_distance(player_position, spawn_position):
@@ -26,6 +26,10 @@ func generate_raiders(proc_gen_world: ProcGenWorld, player_position: Vector2) ->
 		var raider: Raider = RaiderScene.instantiate() 
 		raider.global_position = spawn_position * ProcGenWorld.TILE_SIZE
 		raiders_root.add_child(raider)
+
+
+func _create_raiders_positions(proc_gen_world: ProcGenWorld) -> Array[Vector2i]:
+	return proc_gen_world.sand_arr.filter(func(pos): return not pos in proc_gen_world.cliff_arr)
 
 
 func _in_raider_distance(player_position, spawn_position) -> bool:
